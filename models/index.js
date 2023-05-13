38da0cd7d4cd9b84769d81403aa882a95a33135c
Ian Bui <thinkbui@gmail.com>
2023-05-13 00:19:29 -0500

    Added comment seeds

--- models/index.js
+++ models/index.js
@@ -1,7 +1,16 @@
 const User = require("./User");
 const Other = require("./Other");
+const Post = require("./Post");
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+User.hasMany(Post, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
 
 
 
-
-module.exports = { Other, User }
+module.exports = { Other, User, Post }
