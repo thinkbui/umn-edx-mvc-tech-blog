6ead3e2d4ab923eecafa1a706995cb5a62aedc6a
Ian Bui <thinkbui@gmail.com>
2023-05-13 03:20:05 -0500

    Added partially working dashboard

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,6 +1,25 @@
 const router = require("express").Router()
+const { User, Post } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 /* All html page routes would go here */
+
+router.get('/dashboard', withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      order: [['created_at', 'DESC']],
+    });
+
+    const posts = postData.map((project) => project.get({ plain: true }));
+
+    res.render('dashboard', {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
