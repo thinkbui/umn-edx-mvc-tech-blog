9439bd053a51ecfb2307f7066f573d221a0ebb10
Ian Bui <thinkbui@gmail.com>
2023-05-13 18:29:24 -0500

    Added post comment

--- routes/html/postRoutes.js
+++ routes/html/postRoutes.js
@@ -2,10 +2,9 @@ const router = require("express").Router();
 const {Post, User, Comment} = require("../../models");
 
 router.get('/:id', async (req, res) => {
-  if (req.session.logged_id) {
-    let layout = 'dashboard'
-  } else {
-    let layout = 'main'
+  let layout = 'main'
+  if (req.session.logged_in) {
+    layout = 'dashboard'
   }
   try {
     const postData = await Post.findByPk(req.params.id, {
@@ -23,13 +22,23 @@ router.get('/:id', async (req, res) => {
           attributes: [
             'id',
             'content',
+            'created_at',
           ],
+          include: [
+            {
+              model: User,
+              attributes: [
+                'id',
+                'username',
+              ],
+            }
+          ]
         },
       ],
     });
 
     const post = postData.get({ plain: true });
-    res.render('post', { post });
+    res.render('post', { post, layout: layout });
   } catch (err) {
     console.log(err);
     res.status(500).json(err);
